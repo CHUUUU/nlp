28c08366506606d5d9e091a8775a093dc55bf1e4
lmj <lmj5929naver@gmail.com>
2020-02-28 15:03:01 +0900

    [add]

---
+++ multi_processing_example/built-in functions example/example_4_Manager/example.py
@@ -0,0 +1,29 @@
+from multiprocessing import Manager, Process
+import parmap
+import time
+import os
+
+start = time.time()
+
+def do_something(share_dic, input_arg):
+    process = os.getpid()
+    print("Process id : ", process)
+    share_dic[1] = '1'
+    share_dic['2'] = 2
+    share_dic[0.25] = None
+    input_arg.reverse()
+
+if __name__ == "__main__":
+    manager = Manager()
+    share_dic = manager.dict()
+    input_arg = manager.list(range(10))
+
+    p = Process(target=do_something,
+                args=(share_dic, input_arg))
+
+    p.start()
+    p.join()
+
+    print(str(round(time.time()-start, 2)) + "sec")
+    print(share_dic)
+    print(input_arg)
