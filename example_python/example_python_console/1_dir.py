5bf5d0aa59e288e33111769e2685969478c2e6de
Myeongjae Lee <lmj5929@naver.com>
2020-03-02 00:07:07 +0900

    [add]

---
+++ example_python/example_python_console/1_dir.py
@@ -0,0 +1,12 @@
+import warnings
+print(dir(warnings))
+
+# ['WarningMessage', '_OptionError', '__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name_
+# _', '__package__', '__spec__', '_add_filter', '_defaultaction', '_filters_mutated', '_formatwarning_orig', '_formatwarnms
+# g', '_formatwarnmsg_impl', '_getaction', '_getcategory', '_is_internal_frame', '_next_external_frame', '_onceregistry', '
+# _processoptions', '_setoption', '_showwarning_orig', '_showwarnmsg', '_showwarnmsg_impl', '_warn_unawaited_coroutine', 'c
+# atch_warnings', 'defaultaction', 'filters', 'filterwarnings', 'formatwarning', 'onceregistry', 'resetwarnings', 'showwarn
+# ing', 'simplefilter', 'sys', 'warn', 'warn_explicit']
+
+
+# 콘솔에서 해당 라이브러리 목록을 빠르게 훑어볼 수 있다.
