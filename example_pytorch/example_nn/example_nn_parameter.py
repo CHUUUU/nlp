71d33d949652ef95eaeed7ac7331ba42738bdacf
Myeongjae Lee <lmj5929@naver.com>
2020-03-28 22:47:37 +0900

    [update]

--- example_pytorch/example_nn/example_nn_parameter.py
+++ example_pytorch/example_nn/example_nn_parameter.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 class model_one(nn.Module):
     def __init__(self):
@@ -27,10 +28,20 @@ class model_three(nn.Module):
     def forward(self, x):
         return x @ self.weights + self.bias
 
+class model_four(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.weights = Variable(torch.randn(300, 10))
+        self.bias = Variable(torch.zeros(10))
+
+    def forward(self, x):
+        return x @ self.weights + self.bias
+
 if __name__ == '__main__':
     one = model_one()
     two = model_two()
     three = model_three()
+    four = model_four()
 
     def show_parameters(model, model_name):
         print(model_name)
@@ -40,6 +51,7 @@ if __name__ == '__main__':
     show_parameters(one, "model_one")
     show_parameters(two, "model_two")
     show_parameters(three, "model_three")
+    show_parameters(four, "model_four")
 
     # model_one
     # <class 'torch.Tensor'> torch.Size([300, 10])
@@ -48,4 +60,4 @@ if __name__ == '__main__':
     # <class 'torch.Tensor'> torch.Size([10, 300])
     # <class 'torch.Tensor'> torch.Size([10])
     # model_three
-
+    # model_four
