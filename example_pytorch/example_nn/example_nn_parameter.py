55e6b65aad73740d4ab1bd1c029b5c55edcecff7
Myeongjae Lee <lmj5929@naver.com>
2020-03-28 22:33:55 +0900

    [update]

--- example_pytorch/example_nn/example_nn_parameter.py
+++ example_pytorch/example_nn/example_nn_parameter.py
@@ -18,4 +18,34 @@ class model_two(nn.Module):
     def forward(self, x):
         return self.linear(x)
 
+class model_three(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.weights = torch.randn(300, 10)
+        self.bias = torch.zeros(10)
+
+    def forward(self, x):
+        return x @ self.weights + self.bias
+
+if __name__ == '__main__':
+    one = model_one()
+    two = model_two()
+    three = model_three()
+
+    def show_parameters(model, model_name):
+        print(model_name)
+        for param in model.parameters():
+            print(type(param.data), param.size())
+
+    show_parameters(one, "model_one")
+    show_parameters(two, "model_two")
+    show_parameters(three, "model_three")
+
+    # model_one
+    # <class 'torch.Tensor'> torch.Size([300, 10])
+    # <class 'torch.Tensor'> torch.Size([10])
+    # model_two
+    # <class 'torch.Tensor'> torch.Size([10, 300])
+    # <class 'torch.Tensor'> torch.Size([10])
+    # model_three
 
