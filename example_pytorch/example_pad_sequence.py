c39694f3e84ee4b95d921d466e0ae69d476d17e2
lmj <lmj5929naver@gmail.com>
2020-07-20 13:53:04 +0900

    [update]

---
+++ example_pytorch/example_pad_sequence.py
@@ -0,0 +1,10 @@
+from torch import nn
+import torch
+from torch.nn.utils.rnn import pad_sequence
+
+x_seq = [torch.tensor([5, 18, 29]), torch.tensor([32, 100]), torch.tensor([699, 6, 9, 17])]
+x_padded = pad_sequence(x_seq, batch_first=True, padding_value=0)
+print(x_padded)
+# tensor([[  5,  18,  29,   0],
+#         [ 32, 100,   0,   0],
+#         [699,   6,   9,  17]])
